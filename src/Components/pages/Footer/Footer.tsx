c8e8ee1e80c0f05e0a49ae99fbc4c8285eb48e7f
nazim1971 <md.nazimuddinaj@gmail.com>
2024-08-20 21:39:44 +0600

    start working on footer section

---
+++ src/Components/pages/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            footerr
+        </div>
+    );
+};
+
+export default Footer;
--- src/Components/pages/Root/Root.tsx
+++ src/Components/pages/Root/Root.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 
 const Root = () => {
     return (
         <div className="max-w-[1920px] mx-auto">
           <Outlet/>
+          <Footer/>
         </div>
     );
 };
