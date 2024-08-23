0de58740fa819e95386691fb672a4daed9668d4b
nazim1971 <md.nazimuddinaj@gmail.com>
2024-08-23 23:27:30 +0600

    work on topbar

--- src/Components/pages/Home/Nav.tsx
+++ src/Components/pages/Home/Nav.tsx
@@ -24,13 +24,13 @@ const Nav = () => {
            <div className="">
            <nav className="h-20 flex  lg:w-[1290px] mx-auto justify-between items-center border  ">
             <BsSearch className="col-start-1" />
-            <img className="lg:h-[65px] rounded-xl " src="https://ghorerbazar.com/cdn/shop/files/logo.webp?v=1707766182&width=500" alt="" />
+            <img className=" h-[90%] lg:h-[65px] rounded-xl " src="https://ghorerbazar.com/cdn/shop/files/logo.webp?v=1707766182&width=500" alt="" />
             <FaShoppingBag />
             </nav>
            </div>
                 {/* Nav bottom */}
            <div className="bg-stone-200 "   >
-            <nav className="flex flex-wrap py-4 lg:py-0 lg:w-[1290px] mx-auto lg:h-12 items-center px-3 lg:justify-between">
+            <nav className="flex flex-wrap  py-4 lg:py-0 lg:w-[1290px] mx-auto lg:h-12 items-center gap-4 md:gap-0  px-3 md:justify-between">
            {
             allCat.map((i,idx)=>
             <p key={idx}>
