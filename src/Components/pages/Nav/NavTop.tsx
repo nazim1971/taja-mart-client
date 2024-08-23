a62b859516a4db92d4fab8e2dcd19a1964f644f7
nazim1971 <md.nazimuddinaj@gmail.com>
2024-08-23 23:16:35 +0600

    update nav bellw

--- src/Components/pages/Nav/NavTop.tsx
+++ src/Components/pages/Nav/NavTop.tsx
@@ -4,14 +4,14 @@ import { FaShoppingBag } from "react-icons/fa";
 const NavTop = () => {
     return (
         <div className="bg-slate-200 py-8 absolute w-full">
-            <nav className=" w-[1290px] mx-auto place-items-center grid grid-cols-4 gap-8 justify-items-center">
+            <nav className=" lg:w-[1290px] mx-auto place-items-center grid grid-cols-1 lg:grid-cols-4 gap-8 justify-items-center">
               <img className="h-16" src="https://ghorerbazar.com/cdn/shop/files/logo.webp?v=1707766182&width=500" alt="" />
                 <div className="col-span-2">
                     <div className="relative ">
                         <input className="h-12 w-full border rounded-3xl" type="text" />
                         <BiSearch className="absolute right-5 top-4"  />
                     </div>
-                    <div className="flex gap-3">
+                    <div className="flex flex-wrap gap-3">
                         <p className="text-stone-400">Popular Searches:</p>
                   <a className="hover:underline" href="#">Organic Oil</a>
                   <a className="hover:underline" href="#">Honey</a>
